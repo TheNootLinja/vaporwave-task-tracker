903ade63a8bfce3bd1abbebae468021a1665c657
TheNootLinja <thenootlinja@gmail.com>
2021-05-27 22:16:52 -0400

    Add edit icon to tasks and hook up function that triggers when it is clicked

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,6 +1,6 @@
 import '../styles/Tasks.css';
 
-function Tasks({ taskName, taskDateTime, taskDesc, taskId, onDelete }) {
+function Tasks({ taskName, taskDateTime, taskDesc, taskId, onDelete, onEdit }) {
   return (
     <div className="tasks">
       <div className="task">
@@ -10,6 +10,7 @@ function Tasks({ taskName, taskDateTime, taskDesc, taskId, onDelete }) {
         <p className='task-info'>{taskName}</p>
         <p className='task-info'>{taskDateTime}</p>
         <p className='task-info'>{taskDesc}</p>
+        <img onClick={onEdit} className='edit-icon' src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Red_pencil.svg/768px-Red_pencil.svg.png' alt='' />
       </div>
     </div>
   );
